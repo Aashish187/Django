d9aa94db47e5a2613be56b8f2c75120160813e4c
Aashish187 <aashishsinghnegi0408@gmail.com>
2025-12-14 17:07:14 +0530

    crispy_forms used

--- newspaper_project/newspaper_project/settings.py
+++ newspaper_project/newspaper_project/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'users',
+    'crispy_forms',
+    'crispy_bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -126,3 +128,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL='users.CustomUser'
 LOGIN_REDIRECT_URL='home'
 LOGOUT_REDIRECT_URL='home'
+CRISPY_TEMPLATE_PACK='bootstrap4'
