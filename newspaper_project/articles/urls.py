6477c97c5efecac99bee3c4d7d634bb3a9e8619e
Aashish187 <aashishsinghnegi0408@gmail.com>
2025-12-19 17:53:40 +0530

    delete functionality

--- newspaper_project/articles/urls.py
+++ newspaper_project/articles/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ArticleListView,ArticleUpdateView
+from .views import ArticleListView,ArticleUpdateView,ArticleDeleteView
 urlpatterns=[
     path('',ArticleListView.as_view(),name='article_list'),
     path('<int:pk>/edit/',ArticleUpdateView.as_view(),name='article_edit'),
+    path('<int:pk>/delete/',ArticleDeleteView.as_view(),name='article_delete'),
 ]
--- newspaper_project/articles/views.py
+++ newspaper_project/articles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, CreateView,UpdateView,DeleteView
 from .models import Articles
+from django.urls import reverse_lazy
 # Create your views here.
 class ArticleListView(ListView):
     model=Articles
@@ -11,3 +12,9 @@ class ArticleUpdateView(UpdateView):
     model=Articles
     template_name='article_edit.html'
     fields=['title','body']
+
+class ArticleDeleteView(DeleteView):
+    model=Articles
+    template_name='article_delete.html'
+    success_url=reverse_lazy('article_list')
+
