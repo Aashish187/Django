13b093467b01c02802f6ee87d281003fa1eabcd8
Aashish187 <aashishsinghnegi0408@gmail.com>
2025-12-19 17:45:19 +0530

    edit functionality

--- newspaper_project/articles/urls.py
+++ newspaper_project/articles/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import ArticleListView
+from .views import ArticleListView,ArticleUpdateView
 urlpatterns=[
-    path('',ArticleListView.as_view(),name='article_list')
+    path('',ArticleListView.as_view(),name='article_list'),
+    path('<int:pk>/edit/',ArticleUpdateView.as_view(),name='article_edit'),
 ]
--- newspaper_project/articles/views.py
+++ newspaper_project/articles/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView,UpdateView,DeleteView
 from .models import Articles
 # Create your views here.
 class ArticleListView(ListView):
     model=Articles
     template_name='article_list.html'
+
+
+class ArticleUpdateView(UpdateView):
+    model=Articles
+    template_name='article_edit.html'
+    fields=['title','body']
