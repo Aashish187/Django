7cb742f720ed57aea6d2e42750b97ecd127d5357
Aashish187 <aashishsinghnegi0408@gmail.com>
2025-12-20 12:22:10 +0530

    access denied to logged out users(loginrequiredmixin)

--- newspaper_project/articles/views.py
+++ newspaper_project/articles/views.py
@@ -1,30 +1,36 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, CreateView,UpdateView,DeleteView,DetailView
 from .models import Articles
 from django.urls import reverse_lazy
 # Create your views here.
-class ArticleListView(ListView):
+class ArticleListView(LoginRequiredMixin,ListView):
     model=Articles
     template_name='article_list.html'
+    login_url='login'
 
-class ArticleDetailView(DetailView):
+class ArticleDetailView(LoginRequiredMixin,DetailView):
     model=Articles
     template_name='article_detail.html'
+    login_url='login'
 
-class ArticleUpdateView(UpdateView):
+class ArticleUpdateView(LoginRequiredMixin,UpdateView):
     model=Articles
     template_name='article_edit.html'
     fields=['title','body']
+    login_url='login'
 
-class ArticleDeleteView(DeleteView):
+class ArticleDeleteView(LoginRequiredMixin,DeleteView):
     model=Articles
     template_name='article_delete.html'
     success_url=reverse_lazy('article_list')
+    login_url='login'
 
-class ArticleCreateView(CreateView):
+class ArticleCreateView(LoginRequiredMixin,CreateView):
     model=Articles
     template_name='article_create.html'
     fields=['title','body']
+    login_url='login'
 
     def form_valid(self,form):
         form.instance.author=self.request.user
