a91fd3235599d5e96aec70227077985742035ecc
Aashish187 <aashishsinghnegi0408@gmail.com>
2025-12-20 16:10:18 +0530

    added the comments

--- newspaper_project/articles/admin.py
+++ newspaper_project/articles/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
-from .models import Articles
+from .models import Articles,Comments
 # Register your models here.
-admin.site.register(Articles)
+class CommentInline(admin.StackedInline): #will display the comment objects in short
+    model=Comments  # which model to use
+
+class ArticleAdmin(admin.ModelAdmin): # to customize the articles admin page
+    inlines=[CommentInline,] #     show the commentinline
+
+admin.site.register(Articles,ArticleAdmin) 
+admin.site.register(Comments) 
