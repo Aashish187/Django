2c1b279a33f7be0f51fe6e1c52fdf592294d9a35
Aashish187 <aashishsinghnegi0408@gmail.com>
2025-12-20 11:42:14 +0530

    updated createview for auto author set

--- newspaper_project/articles/views.py
+++ newspaper_project/articles/views.py
@@ -24,4 +24,8 @@ class ArticleDeleteView(DeleteView):
 class ArticleCreateView(CreateView):
     model=Articles
     template_name='article_create.html'
-    fields=['title','author','body']
+    fields=['title','body']
+
+    def form_valid(self,form):
+        form.instance.author=self.request.user
+        return super().form_valid(form)
