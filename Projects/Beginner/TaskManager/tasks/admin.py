75e116da4b3ceda4666a81564a10eab467b51c8c
Aashish187 <aashishsinghnegi0408@gmail.com>
2025-12-26 12:48:54 +0530

    Views created

--- Projects/Beginner/TaskManager/tasks/admin.py
+++ Projects/Beginner/TaskManager/tasks/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Tasks
 # Register your models here.
+"""We will register our Tasks model to the admin"""
+admin.site.register(Tasks)
--- Projects/Beginner/TaskManager/tasks/views.py
+++ Projects/Beginner/TaskManager/tasks/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from .models import Tasks
+from django.urls import reverse_lazy
+""" We will create the views of our Different Pages in the Tasks App"""
 
-# Create your views here.
+from django.views.generic import CreateView,UpdateView,DeleteView,DetailView,ListView
+
+"""
+We will use context_objects names in our list and detail view for easy naming in 
+html files of the object_list datatype.
+"""
+class TaskView(ListView):
+    model=Tasks
+    template_name='task.html'
+    context_object_name='tasks'
+
+class TaskDetailView(DetailView):
+    model=Tasks
+    template_name='task_detail.html'
+    context_object_name='task'
+
+class TaskCreateView(CreateView):
+    model=Tasks
+    fields='__all__'
+    success_url=reverse_lazy('task')
+    template_name='task_create.html'
+
+"""By default after the edit dajngo automatically redirects to the page of the 
+get_absolute_url in our models file.
+"""
+class TaskEditView(UpdateView):
+    model=Tasks
+    fields=['title','description','status','due_date',]
+    template_name='task_edit.html'
+
+class TaskDeleteView(DeleteView):
+    model=Tasks
+    success_url=reverse_lazy('task')
+    template_name='task_delete.html'
